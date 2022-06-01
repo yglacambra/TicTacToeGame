1ad4c1a452f3b8c518ebd0b765b7a44e82e3efa1
unknown <yuangab10@gmail.com>
2022-06-01 22:55:30 +0800

    added a bunch of controls on the form

--- TicTacToeGame/Form1.Designer.cs
+++ TicTacToeGame/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TicTacToeGame
 {
-    partial class Form1
+    partial class TicTacToeGame
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnTopLeft = new System.Windows.Forms.Button();
+            this.BtnTopCntr = new System.Windows.Forms.Button();
+            this.BtnTopRight = new System.Windows.Forms.Button();
+            this.BtnMidLeft = new System.Windows.Forms.Button();
+            this.BtnMidCntr = new System.Windows.Forms.Button();
+            this.BtnMidRight = new System.Windows.Forms.Button();
+            this.BtnBotLeft = new System.Windows.Forms.Button();
+            this.BtnBotCntr = new System.Windows.Forms.Button();
+            this.BtnBotRight = new System.Windows.Forms.Button();
+            this.LblPlayer1 = new System.Windows.Forms.Label();
+            this.LblPlayer2 = new System.Windows.Forms.Label();
+            this.TxtBoxPlyr2Name = new System.Windows.Forms.TextBox();
+            this.TxtBoxPlyr1Name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnTopLeft
+            // 
+            this.BtnTopLeft.Location = new System.Drawing.Point(237, 77);
+            this.BtnTopLeft.Name = "BtnTopLeft";
+            this.BtnTopLeft.Size = new System.Drawing.Size(70, 71);
+            this.BtnTopLeft.TabIndex = 0;
+            this.BtnTopLeft.UseVisualStyleBackColor = true;
+            // 
+            // BtnTopCntr
+            // 
+            this.BtnTopCntr.Location = new System.Drawing.Point(303, 77);
+            this.BtnTopCntr.Name = "BtnTopCntr";
+            this.BtnTopCntr.Size = new System.Drawing.Size(70, 71);
+            this.BtnTopCntr.TabIndex = 1;
+            this.BtnTopCntr.UseVisualStyleBackColor = true;
+            // 
+            // BtnTopRight
+            // 
+            this.BtnTopRight.Location = new System.Drawing.Point(371, 77);
+            this.BtnTopRight.Name = "BtnTopRight";
+            this.BtnTopRight.Size = new System.Drawing.Size(70, 71);
+            this.BtnTopRight.TabIndex = 2;
+            this.BtnTopRight.UseVisualStyleBackColor = true;
+            // 
+            // BtnMidLeft
+            // 
+            this.BtnMidLeft.Location = new System.Drawing.Point(237, 146);
+            this.BtnMidLeft.Name = "BtnMidLeft";
+            this.BtnMidLeft.Size = new System.Drawing.Size(70, 71);
+            this.BtnMidLeft.TabIndex = 3;
+            this.BtnMidLeft.UseVisualStyleBackColor = true;
+            // 
+            // BtnMidCntr
+            // 
+            this.BtnMidCntr.Location = new System.Drawing.Point(303, 146);
+            this.BtnMidCntr.Name = "BtnMidCntr";
+            this.BtnMidCntr.Size = new System.Drawing.Size(70, 71);
+            this.BtnMidCntr.TabIndex = 4;
+            this.BtnMidCntr.UseVisualStyleBackColor = true;
+            // 
+            // BtnMidRight
+            // 
+            this.BtnMidRight.Location = new System.Drawing.Point(371, 146);
+            this.BtnMidRight.Name = "BtnMidRight";
+            this.BtnMidRight.Size = new System.Drawing.Size(70, 71);
+            this.BtnMidRight.TabIndex = 5;
+            this.BtnMidRight.UseVisualStyleBackColor = true;
+            // 
+            // BtnBotLeft
+            // 
+            this.BtnBotLeft.Location = new System.Drawing.Point(237, 215);
+            this.BtnBotLeft.Name = "BtnBotLeft";
+            this.BtnBotLeft.Size = new System.Drawing.Size(70, 71);
+            this.BtnBotLeft.TabIndex = 6;
+            this.BtnBotLeft.UseVisualStyleBackColor = true;
+            // 
+            // BtnBotCntr
+            // 
+            this.BtnBotCntr.Location = new System.Drawing.Point(303, 215);
+            this.BtnBotCntr.Name = "BtnBotCntr";
+            this.BtnBotCntr.Size = new System.Drawing.Size(70, 71);
+            this.BtnBotCntr.TabIndex = 7;
+            this.BtnBotCntr.UseVisualStyleBackColor = true;
+            // 
+            // BtnBotRight
+            // 
+            this.BtnBotRight.Location = new System.Drawing.Point(371, 215);
+            this.BtnBotRight.Name = "BtnBotRight";
+            this.BtnBotRight.Size = new System.Drawing.Size(70, 71);
+            this.BtnBotRight.TabIndex = 8;
+            this.BtnBotRight.UseVisualStyleBackColor = true;
+            // 
+            // LblPlayer1
+            // 
+            this.LblPlayer1.AutoSize = true;
+            this.LblPlayer1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblPlayer1.Location = new System.Drawing.Point(64, 11);
+            this.LblPlayer1.Name = "LblPlayer1";
+            this.LblPlayer1.Size = new System.Drawing.Size(97, 25);
+            this.LblPlayer1.TabIndex = 9;
+            this.LblPlayer1.Text = "Player 1";
+            // 
+            // LblPlayer2
+            // 
+            this.LblPlayer2.AutoSize = true;
+            this.LblPlayer2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblPlayer2.Location = new System.Drawing.Point(506, 11);
+            this.LblPlayer2.Name = "LblPlayer2";
+            this.LblPlayer2.Size = new System.Drawing.Size(97, 25);
+            this.LblPlayer2.TabIndex = 10;
+            this.LblPlayer2.Text = "Player 2";
+            // 
+            // TxtBoxPlyr2Name
+            // 
+            this.TxtBoxPlyr2Name.Location = new System.Drawing.Point(477, 39);
+            this.TxtBoxPlyr2Name.Name = "TxtBoxPlyr2Name";
+            this.TxtBoxPlyr2Name.Size = new System.Drawing.Size(154, 23);
+            this.TxtBoxPlyr2Name.TabIndex = 12;
+            // 
+            // TxtBoxPlyr1Name
+            // 
+            this.TxtBoxPlyr1Name.Location = new System.Drawing.Point(39, 39);
+            this.TxtBoxPlyr1Name.Name = "TxtBoxPlyr1Name";
+            this.TxtBoxPlyr1Name.Size = new System.Drawing.Size(154, 23);
+            this.TxtBoxPlyr1Name.TabIndex = 13;
+            // 
+            // TicTacToeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(667, 337);
+            this.Controls.Add(this.TxtBoxPlyr1Name);
+            this.Controls.Add(this.TxtBoxPlyr2Name);
+            this.Controls.Add(this.LblPlayer2);
+            this.Controls.Add(this.LblPlayer1);
+            this.Controls.Add(this.BtnBotRight);
+            this.Controls.Add(this.BtnBotCntr);
+            this.Controls.Add(this.BtnBotLeft);
+            this.Controls.Add(this.BtnMidRight);
+            this.Controls.Add(this.BtnMidCntr);
+            this.Controls.Add(this.BtnMidLeft);
+            this.Controls.Add(this.BtnTopRight);
+            this.Controls.Add(this.BtnTopCntr);
+            this.Controls.Add(this.BtnTopLeft);
+            this.MaximizeBox = false;
+            this.Name = "TicTacToeGame";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button BtnTopLeft;
+        private Button BtnTopCntr;
+        private Button BtnTopRight;
+        private Button BtnMidLeft;
+        private Button BtnMidCntr;
+        private Button BtnMidRight;
+        private Button BtnBotLeft;
+        private Button BtnBotCntr;
+        private Button BtnBotRight;
+        private Label LblPlayer1;
+        private Label LblPlayer2;
+        private TextBox TxtBoxPlyr2Name;
+        private TextBox TxtBoxPlyr1Name;
     }
 }
--- TicTacToeGame/Form1.cs
+++ TicTacToeGame/Form1.cs
@@ -1,8 +1,8 @@
 namespace TicTacToeGame
 {
-    public partial class Form1 : Form
+    public partial class TicTacToeGame : Form
     {
-        public Form1()
+        public TicTacToeGame()
         {
             InitializeComponent();
         }
