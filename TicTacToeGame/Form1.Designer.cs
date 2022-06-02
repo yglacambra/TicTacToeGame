44ae58213b076249228aeca0db7511ba8caa354d
Yuan Gabriel Lacambra <yuangabrielslacambra@iskolarngbayan.pup.edu.ph>
2022-06-02 15:40:38 +0800

    added code for when the remaining buttons are clicked and changed the font and fontsize of the text that will appear when they are clicked

--- TicTacToeGame/Form1.Designer.cs
+++ TicTacToeGame/Form1.Designer.cs
@@ -46,15 +46,17 @@
             // BtnTopLeft
             // 
             this.BtnTopLeft.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnTopLeft.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnTopLeft.Location = new System.Drawing.Point(237, 77);
             this.BtnTopLeft.Name = "BtnTopLeft";
             this.BtnTopLeft.Size = new System.Drawing.Size(70, 71);
             this.BtnTopLeft.TabIndex = 0;
-            this.BtnTopLeft.UseVisualStyleBackColor = false;
+            this.BtnTopLeft.UseVisualStyleBackColor = true;
             this.BtnTopLeft.Click += new System.EventHandler(this.BtnTopLeft_Click);
             // 
             // BtnTopCntr
             // 
+            this.BtnTopCntr.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnTopCntr.Location = new System.Drawing.Point(303, 77);
             this.BtnTopCntr.Name = "BtnTopCntr";
             this.BtnTopCntr.Size = new System.Drawing.Size(70, 71);
@@ -64,59 +66,73 @@
             // 
             // BtnTopRight
             // 
+            this.BtnTopRight.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnTopRight.Location = new System.Drawing.Point(371, 77);
             this.BtnTopRight.Name = "BtnTopRight";
             this.BtnTopRight.Size = new System.Drawing.Size(70, 71);
             this.BtnTopRight.TabIndex = 2;
             this.BtnTopRight.UseVisualStyleBackColor = true;
+            this.BtnTopRight.Click += new System.EventHandler(this.BtnTopRight_Click);
             // 
             // BtnMidLeft
             // 
+            this.BtnMidLeft.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnMidLeft.Location = new System.Drawing.Point(237, 146);
             this.BtnMidLeft.Name = "BtnMidLeft";
             this.BtnMidLeft.Size = new System.Drawing.Size(70, 71);
             this.BtnMidLeft.TabIndex = 3;
             this.BtnMidLeft.UseVisualStyleBackColor = true;
+            this.BtnMidLeft.Click += new System.EventHandler(this.BtnMidLeft_Click);
             // 
             // BtnMidCntr
             // 
+            this.BtnMidCntr.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnMidCntr.Location = new System.Drawing.Point(303, 146);
             this.BtnMidCntr.Name = "BtnMidCntr";
             this.BtnMidCntr.Size = new System.Drawing.Size(70, 71);
             this.BtnMidCntr.TabIndex = 4;
             this.BtnMidCntr.UseVisualStyleBackColor = true;
+            this.BtnMidCntr.Click += new System.EventHandler(this.BtnMidCntr_Click);
             // 
             // BtnMidRight
             // 
+            this.BtnMidRight.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnMidRight.Location = new System.Drawing.Point(371, 146);
             this.BtnMidRight.Name = "BtnMidRight";
             this.BtnMidRight.Size = new System.Drawing.Size(70, 71);
             this.BtnMidRight.TabIndex = 5;
             this.BtnMidRight.UseVisualStyleBackColor = true;
+            this.BtnMidRight.Click += new System.EventHandler(this.BtnMidRight_Click);
             // 
             // BtnBotLeft
             // 
+            this.BtnBotLeft.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnBotLeft.Location = new System.Drawing.Point(237, 215);
             this.BtnBotLeft.Name = "BtnBotLeft";
             this.BtnBotLeft.Size = new System.Drawing.Size(70, 71);
             this.BtnBotLeft.TabIndex = 6;
             this.BtnBotLeft.UseVisualStyleBackColor = true;
+            this.BtnBotLeft.Click += new System.EventHandler(this.BtnBotLeft_Click);
             // 
             // BtnBotCntr
             // 
+            this.BtnBotCntr.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnBotCntr.Location = new System.Drawing.Point(303, 215);
             this.BtnBotCntr.Name = "BtnBotCntr";
             this.BtnBotCntr.Size = new System.Drawing.Size(70, 71);
             this.BtnBotCntr.TabIndex = 7;
             this.BtnBotCntr.UseVisualStyleBackColor = true;
+            this.BtnBotCntr.Click += new System.EventHandler(this.BtnBotCntr_Click);
             // 
             // BtnBotRight
             // 
+            this.BtnBotRight.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BtnBotRight.Location = new System.Drawing.Point(371, 215);
             this.BtnBotRight.Name = "BtnBotRight";
             this.BtnBotRight.Size = new System.Drawing.Size(70, 71);
             this.BtnBotRight.TabIndex = 8;
             this.BtnBotRight.UseVisualStyleBackColor = true;
+            this.BtnBotRight.Click += new System.EventHandler(this.BtnBotRight_Click);
             // 
             // LblPlayer1
             // 
--- TicTacToeGame/Form1.cs
+++ TicTacToeGame/Form1.cs
@@ -7,10 +7,6 @@ namespace TicTacToeGame
         {
             InitializeComponent();
         }
-        private void TicTacToeGame_Load(object sender, EventArgs e)
-        {
-        }
-        
         private void TxtBoxPlyr1Name_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
@@ -75,7 +71,130 @@ namespace TicTacToeGame
 
         private void BtnTopCntr_Click(object sender, EventArgs e)
         {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnTopCntr.Text = "O";
+                BtnTopCntr.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnTopCntr.Text = "X";
+                BtnTopCntr.Enabled = false;
+            }
+        }
 
+        private void BtnTopRight_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnTopRight.Text = "O";
+                BtnTopRight.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnTopRight.Text = "X";
+                BtnTopRight.Enabled = false;
+            }
+        }
+
+        private void BtnMidLeft_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnMidLeft.Text = "O";
+                BtnMidLeft.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnMidLeft.Text = "X";
+                BtnMidLeft.Enabled = false;
+            }
+        }
+
+        private void BtnMidCntr_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnMidCntr.Text = "O";
+                BtnMidCntr.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnMidCntr.Text = "X";
+                BtnMidCntr.Enabled = false;
+            }
+        }
+
+        private void BtnMidRight_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnMidRight.Text = "O";
+                BtnMidRight.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnMidRight.Text = "X";
+                BtnMidRight.Enabled = false;
+            }
+        }
+
+        private void BtnBotLeft_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnBotLeft.Text = "O";
+                BtnBotLeft.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnBotLeft.Text = "X";
+                BtnBotLeft.Enabled = false;
+            }
+        }
+
+        private void BtnBotCntr_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnBotCntr.Text = "O";
+                BtnBotCntr.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnBotCntr.Text = "X";
+                BtnBotCntr.Enabled = false;
+            }
+        }
+
+        private void BtnBotRight_Click(object sender, EventArgs e)
+        {
+            if (Turn % 2 == 1)
+            {
+                ++Turn;
+                BtnBotRight.Text = "O";
+                BtnBotRight.Enabled = false;
+            }
+            else
+            {
+                ++Turn;
+                BtnBotRight.Text = "X";
+                BtnBotRight.Enabled = false;
+            }
         }
     }
 }
